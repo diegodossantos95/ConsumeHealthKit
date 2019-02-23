3531aa79568ffe014e3b5b00c71342b0a6c32d35
dos Santos, Diego <diego.dos.santos@sap.com>
2019-02-22 21:45:52 -0300

    Added base project

---
+++ ConsumeHealthKit/ConsumeHealthKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConsumeHealthKit
+//
+//  Created by dos Santos, Diego on 22/02/19.
+//  Copyright © 2019 Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
