37cca14e5f9480acfba89ef2047b678f13aaaa73
dos Santos, Diego <diego.dos.santos@sap.com>
2019-02-23 00:38:20 -0300

    Created HealthKitService to read heart beats

---
+++ ConsumeHealthKit/ConsumeHealthKit/Service/HealthKitService.swift
@@ -0,0 +1,56 @@
+//
+//  HealthKitService.swift
+//  ConsumeHealthKit
+//
+//  Created by dos Santos, Diego on 22/02/19.
+//  Copyright © 2019 Diego. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthKitService {
+    let healthKitDataStore: HKHealthStore?
+    let readableHKCharacteristicTypes: Set<HKQuantityType>?
+
+    init() {
+        if HKHealthStore.isHealthDataAvailable() {
+            healthKitDataStore = HKHealthStore()
+            readableHKCharacteristicTypes = [HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!]
+
+            healthKitDataStore?.requestAuthorization(toShare: nil,
+                                                     read: readableHKCharacteristicTypes,
+                                                     completion: authRequestCompletion)
+
+        } else {
+            healthKitDataStore = nil
+            readableHKCharacteristicTypes = nil
+        }
+    }
+
+    func authRequestCompletion(success: Bool, error: Error?) {
+        if success {
+            print("Successful authorization.")
+        } else {
+            print(error.debugDescription)
+        }
+    }
+
+    func readHeartRateData() {
+        let heartRateType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
+
+        let query = HKAnchoredObjectQuery(type: heartRateType, predicate: nil, anchor: nil, limit: HKObjectQueryNoLimit) {
+            (query, samplesOrNil, deletedObjectsOrNil, newAnchor, errorOrNil) in
+
+            if let samples = samplesOrNil {
+                for heartRateSamples in samples {
+                    print(heartRateSamples)
+                }
+            } else {
+                print("No heart rate sample available.")
+            }
+        }
+
+        healthKitDataStore?.execute(query)
+    }
+}
--- ConsumeHealthKit/ConsumeHealthKit/ViewControllers/ViewController.swift
+++ ConsumeHealthKit/ConsumeHealthKit/ViewControllers/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var healthKitService: HealthKitService?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        healthKitService = HealthKitService()
+        healthKitService?.readHeartRateData()
     }
-
-
 }
-
